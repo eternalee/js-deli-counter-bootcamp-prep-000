a22808a7d3d0d357d1a7330027d1b6a97d5ada16
Eterna <one.eternity@gmail.com>
2018-01-10 11:16:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+var katzDeliLine = []
+
+function takeANumber(katzDeliLine, newName) {
+  katzDeliLine.push(newName);
+  return "Welcome, "+newName+". You are number "+katzDeliLine.length+" in line.";
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  }
+  else {
+    return "Currently serving " +katzDeliLine.shift()+".";  
+  }
+  
+}
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length ===0) {
+    return "The line is currently empty."
+  }
+  else {
+  var string = "The line is currently 1. ${katzDeliLine[0]}"
+  for (let i=0; i<katzDeliLine.length, i++) {
+    string.push(i+1)
+  }
+  return string;  
+  }
+}
